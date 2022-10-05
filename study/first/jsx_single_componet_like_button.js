023fd0a6aab9dbdc476fd80b6c2854d398fd312d
ciple <jade9877@hanmail.net>
2022-10-05 14:46:46 +0900

    first commit

---
+++ study/first/jsx_single_componet_like_button.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return '좋아요 클릭!';
+    }
+
+    // return e(
+    //   'button',
+    //   { onClick: () => this.setState({ liked: true }) },
+    //   '좋아요'
+    // );
+
+    return (
+        <button onClick={() => this.setState({ liked: true })}>
+          Like
+        </button>
+      );
+  }
+}
+
+// const domContainer = document.querySelector('#like_button_container');
+const domContainer = document.getElementById("like_button_container");
+
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
