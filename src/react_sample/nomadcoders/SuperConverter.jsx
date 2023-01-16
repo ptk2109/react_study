72c8b0b38e39db7280008e9ece0f92c9a42adef9
ciple <jade9877@hanmail.net>
2023-01-16 23:23:49 +0900

    단위 변환기 완료

--- src/react_sample/nomadcoders/SuperConverter.jsx
+++ src/react_sample/nomadcoders/SuperConverter.jsx
@@ -2,15 +2,109 @@ import React, { useState } from 'react';
 import LeftMenu from "./leftmenu/LeftMenu"
 
 
-function Base(props) {
+/**
+ * 분/시간 변환 컴포넌트
+ */
+function MinutesToHours(){
+	const [amount, setAmount] = useState();
+	const [fliped, setFliped] = useState(false);
 
-	const [minutes, setMinutes] = useState();
 
-	const onChangeMinutes = (event) => {
-		setMinutes(event.target.value);
+	const onChangeAmount = (event) => {
+		setAmount(event.target.value);
 	}
 
-	const onReset = () => setMinutes(0);
+	const onFlip = () => {
+		onReset();
+		setFliped( (current) => !current);
+	}
+
+
+	const onReset = () => setAmount(0);
+
+	return (
+		<>
+			<div>
+				<label>
+					<span style={{ width: "60px", display: 'inline-block' }}>Minutes</span>
+					<input placeholder='Minutes' type="number" disabled={fliped} value={fliped ? amount * 60 : amount} onChange={onChangeAmount} />
+				</label>
+			</div>
+
+			<div>
+				<label>
+					<span style={{ width: "60px", display: 'inline-block' }}>Hours</span>
+					<input placeholder='Hours' type="number" disabled={!fliped} value={!fliped ? Math.round(amount / 60) : amount} onChange={onChangeAmount} />
+				</label>
+			</div>
+
+
+			<div>
+				<button onClick={onReset}>Reset</button>
+				<button onClick={onFlip}>Flip</button>
+			</div>
+		</>
+	);
+}
+
+
+/**
+ * km/mile 변환 컴포넌트
+ */
+function KmToMiles(){
+	const [amount, setAmount] = useState();
+	const [fliped, setFliped] = useState(false);
+
+
+	const onChangeAmount = (event) => {
+		setAmount(event.target.value);
+	}
+
+	const onFlip = () => {
+		onReset();
+		setFliped( (current) => !current);
+	}
+
+
+	const onReset = () => setAmount(0);
+
+	return (
+		<>
+			<div>
+				<label>
+					<span style={{ width: "60px", display: 'inline-block' }}>Km</span>
+					<input placeholder='Km' type="number" disabled={fliped} value={fliped ? amount * 1.609344 : amount} onChange={onChangeAmount} />
+				</label>
+			</div>
+
+			<div>
+				<label>
+					<span style={{ width: "60px", display: 'inline-block' }}>Miles</span>
+					<input placeholder='Miles' type="number" disabled={!fliped} value={!fliped ? (amount / 1.609344).toFixed(10) : amount} onChange={onChangeAmount} />
+				</label>
+			</div>
+
+
+			<div>
+				<button onClick={onReset}>Reset</button>
+				<button onClick={onFlip}>Flip</button>
+			</div>
+		</>
+	);
+}
+
+
+/**
+ * 기본 컨포넌트
+ */
+function Base(props) {
+
+	const [index, setIndex] = useState("xx");
+
+	const onSelect = (event) => {
+		setIndex(event.target.value);
+	}
+	
 	return (
 		<>
 			<LeftMenu />
@@ -19,27 +113,25 @@ function Base(props) {
 				<div className="section">
 					<div className="sub_title">Super Converter</div>
 					<div className="discription">
-						- Minutes 입력하면 시간으로 변환하여 Hours에 출력 <br />
-					</div>
-
-					<div>
-						<label>
-							<span style={{ width: "60px", display: 'inline-block' }}>Minutes</span>
-							<input placeholder='Minutes' type="number" value={minutes} onChange={onChangeMinutes} />
-						</label>
-					</div>
-
-					<div>
-						<label>
-							<span style={{ width: "60px", display: 'inline-block' }}>Hours</span>
-							<input placeholder='Hours' type="number" value={Math.round(minutes / 60)} />
-						</label>
+						<b>[학습목표]</b>
+						- state를 사용할 수 있다. <br />
+						- state로 prop 흉내내본다.<br />
 					</div>
 
 
-					<div>
-						<button onClick={onReset}>Reset</button>
-					</div>
+					<select value={index} onChange={onSelect}>
+						<option value="xx">::선택::</option>
+						<option value="0">Minute & Hour</option>
+						<option value="1">Km & Mile</option>
+					</select>
+
+					<hr />
+
+					{index === "xx" ? "선택해주세요" : null}
+					{index === "0" ? <MinutesToHours /> : null}
+					{index === "1" ? <KmToMiles /> : null}
+
+					
 				</div>
 			</div>
 		</>
