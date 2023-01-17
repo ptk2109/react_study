ac1b66b1485e70d2bcc56516c0e4c8bf5b2db391
ciple <jade9877@hanmail.net>
2023-01-18 00:19:20 +0900

    [노마드] prop 샘플 완료

--- src/react_sample/nomadcoders/PropSample.jsx
+++ src/react_sample/nomadcoders/PropSample.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import LeftMenu from "./leftmenu/LeftMenu"
-
+import PropTypes from 'prop-types';
 
 
 function Btn({name, big=false}){
@@ -20,17 +20,60 @@ function Btn({name, big=false}){
 
 
 
+function Btn2({name, onClick}){
+	console.log(`${name} : `, onClick);
+	return(
+		<button 
+			onClick={onClick}
+			style={{
+				backgroundColor: "tomato",
+				color:"white",
+				padding: "10px 20px",
+				border: 0,
+				borderRadius: 10
+			}}
+		>{name}</button>
+	);
+}
+const MemorizedBtn2 = React.memo(Btn2);
+
+
+
+function Btn3({name, fontSize}){
+	return(
+		<button 
+			style={{
+				backgroundColor: "tomato",
+				color:"white",
+				padding: "10px 20px",
+				border: 0,
+				borderRadius: 10,
+				fontSize
+			}}
+		>{name}</button>
+	);
+}
+Btn3.propTypes = {
+	text : PropTypes.string.isRequired,
+	fontSize : PropTypes.number,
+}
+
 /**
  * 기본 컨포넌트
  */
 function Base(props) {
+
+	const [btnName, setBtnName] = useState("Save Btn");
+	const changeValue = () => setBtnName("change btn name");
+
+	
 	return (
 		<>
 			<LeftMenu />
 
 			<div className="content_wrap">
 				<div className="section">
-					<div className="sub_title">Super Converter</div>
+					<div className="sub_title">prop 예제1</div>
 					<div className="discription">
 						<b>[학습목표]</b>
 						- prop를 사용할 수 있다. <br />
@@ -39,8 +82,39 @@ function Base(props) {
 					<Btn name="Save Btn" big={true}/>
 					<Btn name="Continue"/>
 					
+				</div> 
+
+
+				<div className="section">
+					<div className="sub_title">prop 예제2</div>
+					<div className="discription">
+						<b>[학습목표]</b>
+						1. 리스너를 형태로 넘겨도 결국은 prop 이다.<br /> 
+						2. prop를 이용하여 함수를 넘길 수도 있다.<br />
+						3. 불필요한 re-render 하는 방법을 배운다.
+					</div>
+
+					<MemorizedBtn2 name={btnName} onClick={changeValue}/> 
+					<MemorizedBtn2 name="Continue"/>												
+					
+				</div>
+
+
+				<div className="section">
+					<div className="sub_title">prop 예제2</div>
+					<div className="discription">
+						<b>[학습목표]</b>
+						1. PropTypes으로 prop 값 예외처리 하는 방법을 배운다. 
+					</div>
+
+					<Btn3 name={btnName} fontSize={18}/> 
+					<Btn3 name={18} fontSize={"text"}/> 
+					<Btn3  fontSize={"text2"}/> 
+					
 				</div>
 			</div>
+
+
 		</>
 	)
 
