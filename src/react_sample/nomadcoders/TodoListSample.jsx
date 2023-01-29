4751c37e584f3cb1872bac3fab0f22cb92c0723e
ciple <jade9877@hanmail.net>
2023-01-29 10:50:36 +0900

    [노마더] 간단 todo 리스트

--- src/react_sample/nomadcoders/TodoListSample.jsx
+++ src/react_sample/nomadcoders/TodoListSample.jsx
@@ -4,16 +4,58 @@ import LeftMenu from "./leftmenu/LeftMenu"
 
 
 
+function ToDoList(){
+	const [toDos, setToDos] = useState([]);
+	const [toDo, setToDo] = useState("");
+
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if(toDo === "") return;
+
+		setToDo("");
+		setToDos( (eventArray) => [toDo, ...toDos] );
+	}
+	const onReset = () => {
+		setToDo("");
+		setToDos([]);
+	}
+
+	return (
+		<div className="content_wrap">
+			<div className="section">
+				<div className="sub_title">todo 리스트 만들기</div>
+				<div className="discription">
+					<b>[학습목표]</b>
+				</div>
+				<form onSubmit={onSubmit}>
+					<h1>todo list ({toDos.length})</h1>
+					<input type="text" onChange={onChange} value={toDo} placeholder={"입력하세요."} />
+					<button  >등록</button>
+					<button type="button" onClick={onReset}>초기화</button>
+
+					{toDos.map( (_todo, _idx) => (
+							<li key={_idx}>{_todo}</li>
+							
+						))
+					}
+				</form>
+			</div>
+		</div>
+	)
+}
+
 /**
  * 기본 컨포넌트
  */
-function Base(props) {
+function Base() {
+	
 
 	return (
 		<>
 			<LeftMenu />
 
-			
+			<ToDoList />
 		</>
 	)
 
