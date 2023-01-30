f1e86f50eeef36debfc03332b289c5c45a8ce24a
ciple <jade9877@hanmail.net>
2023-01-30 21:05:54 +0900

    [노마더] ajax로 모든 코인 정보 불러와서 출력한다.

--- src/react_sample/nomadcoders/CoinTrackerSample.jsx
+++ src/react_sample/nomadcoders/CoinTrackerSample.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import LeftMenu from "./leftmenu/LeftMenu"
 
 
@@ -8,7 +9,22 @@ import LeftMenu from "./leftmenu/LeftMenu"
  * 기본 컨포넌트
  */
 function Base() {
-	
+	const [isLoading, setIsLoading] = useState(false);
+	const [coins, setConins] = useState([]);
+
+	useEffect( () => {
+
+		fetch("https://api.coinpaprika.com/v1/tickers")
+			.then((response) => response.json())
+			.then((json) => {
+				setConins(json);
+				setIsLoading(true);
+			}
+		);
+
+
+
+	}, []);
 
 	return (
 		<>
@@ -18,9 +34,34 @@ function Base() {
 			<div className="section">
 				<div className="sub_title">코인 트래커</div>
 				<div className="discription">
-					<b>[학습목표]</b> <br />
-					- useEffect 활용
+					<b>[기능설명]</b> <br />
+					- ajax로 모든 코인 정보 불러와서 출력한다.
 				</div>
+
+				<h1>The Coin! ({coins.length.toLocaleString()})</h1>
+				
+				{
+					isLoading ? 
+						<table border={'1'}>
+							<tr>
+								<td>번호</td>
+								<td>랭크</td>
+								<td>코인이름</td>
+								<td>USD</td>
+							</tr>
+							{coins.map( 
+								(_coin, _idx) => (
+									<tr>
+										<td>{_idx+1}</td>
+										<td>{_coin.rank}</td>
+										<td>{_coin.name}</td>
+										<td>{_coin.quotes.USD.price}</td>
+									</tr>
+								) 
+							) }
+							</table>
+					: "Loading..."
+				}
 			</div>
 		</div>
 		</>
